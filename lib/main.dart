899872174043569627d136f145d18a531ef4e183
Emmanuel Sanchez <fede@fede.website>
2022-01-07 13:16:35 -0600

    Addign Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s is your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first app'),
-        ),
-        body: Text('This is my default text'),
-      ),
+          appBar: AppBar(
+            title: Text('My first app'),
+          ),
+          body: Column(
+            children: [
+              Text('The question'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
     );
   }
 }
